b9f6b1d961643a1f9ec3e95e4c33a11a5497d9f1
Mark Flajole <mflajole@hotmail.com>
2019-04-29 22:26:21 -0400

    Next commit

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+var sensorValue = "None";
 
 app.get(['/','/iot_capstone/'], function(req, res) {
   var fn=path.join(__dirname,"index.html");
   //console.log("Variable: " + req.params.sample);
   console.log("User logged on at " + new Date());
-  io.sockets.emit('boom', {result: req.params.sample});
   res.sendFile(fn);
 });
 
-app.head(['/:sample','/iot_capstone/:sample'], function(req, res) {
-  console.log("HEAD request, Variable: " + req.params.sample);
-  io.sockets.emit('boom', {result: req.params.sample});
+app.get(['/command','/iot_capstone/command'], function(req, res) {
+  console.log("User logged on at " + new Date());
+  res.send("cmd=siren");
+});
+
+
+app.head(['/sensor/:sensorValue','/iot_capstone/sensor/:sensorValue'], function(req, res) {
+  sensorValue = req.params.sensorValue;
+  console.log("HEAD request, Variable: " + sensorValue);
+  io.sockets.emit('boom', {result: sensorValue});
   res.end();
 });
 
@@ -22,6 +29,7 @@ io.on('connection', function(socket) {
   socket.on('disconnect', function() {
     console.log('A user just disconnected.');
   });
+  socket.send(sensorValue);
 
 });
 
